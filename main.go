1d7283cdaac942bd5a1a57ed37d7ebf574dc78aa
NishDaswani <n.h.daswani@student.tue.nl>
2026-04-21 18:13:44 +0200

    Health check + changed root

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := &http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
