06ac1597a6090997ff609cef02d8fac8298e3017
NishDaswani <n.h.daswani@student.tue.nl>
2026-04-22 09:56:57 +0200

    Added custom handler logic

--- main.go
+++ main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Printf("%s %s (hit #%d)", r.Method, r.URL.Path, cfg.fileserverHits.Load())
+		next.ServeHTTP(w, r)
 	})
-	server := &http.Server{Handler: mux, Addr: ":8080"}
-	server.ListenAndServe()
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.resetMetrics)
+
+	server := &http.Server{Handler: mux, Addr: ":" + port}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
